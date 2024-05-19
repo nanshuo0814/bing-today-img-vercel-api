04e1b01a76791d883d0fa5dfdae5c693f89a4696
nanshuo0814 <nanshuo.icu@qq.com>
2024-05-19 05:03:05 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ api.py
@@ -0,0 +1,41 @@
+import requests, time
+
+
+def bing(idx, UHD):
+    # https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN
+    idxurl = 'https://cn.bing.com'
+    if idx != None:
+        url = idxurl + '/HPImageArchive.aspx?format=js&idx=' + idx + '&n=1&mkt=zh-CN'
+    else:
+        url = idxurl + '/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN'
+    print('url:', url)
+    data = requests.get(url).json()
+    print(data)
+    imgurl = data["images"][0]["url"]
+    if UHD != None:
+        imgurl = imgurl.replace('_1920x1080', '_UHD')
+    returl = 'https://cn.bing.com' + imgurl
+    print('imgurl:', returl)
+    return returl
+
+def info():
+    cn_url = 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN'
+    en_url = 'https://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN'
+    # print('url:', url)
+    开始时间 = time.time()
+    data = requests.get(cn_url).json()
+    结束时间 = time.time()
+    print(data)
+    中国节点用时 = 结束时间 - 开始时间
+    开始时间 = time.time()
+    data = requests.get(en_url).json()
+    结束时间 = time.time()
+    print(data)
+    全球节点用时 = 结束时间 - 开始时间
+    msg = '''info：
+bing壁纸api请求用时
+
+中国: {中国节点用时} s
+
+全球: {全球节点用时} s'''.format(中国节点用时 = 中国节点用时, 全球节点用时 = 全球节点用时)
+    return msg
---
+++ index.py
@@ -0,0 +1,30 @@
+from flask import Flask, redirect, request, jsonify
+from flask_cors import CORS
+import requests
+import api
+# import requests
+app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False
+cors = CORS(app)
+
+@app.route('/')
+def index():
+   类型 = request.args.get('api')
+   if 类型 == 'bing':
+      idx = request.args.get('idx')
+      uhd = request.args.get('uhd')
+      returl = api.bing(idx, uhd)
+      return redirect(returl)
+   elif 类型 == 'info':
+      return api.info()
+   else:
+      return redirect('https://api.cxl2020mc.top?api=bing')
+   
+@app.route('/ip/')
+def get_ip():
+   ip = request.remote_addr
+   data = requests.get("https://api.live.bilibili.com/client/v1/Ip/getInfoNew?ip=" + ip).json()
+   return jsonify(data)
+
+if __name__ == '__main__':
+    app.run()
